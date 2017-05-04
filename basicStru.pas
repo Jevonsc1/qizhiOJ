c9bbd149d69525fd008773db07db6c703a59c325
Jevons <Jevons@jevons-iMac.local>
2017-05-04 09:08:26 +0800

    todo：整数算术，输入输出介绍

---
+++ basicStru.pas
@@ -0,0 +1,11 @@
+{
+    程序的基本结构
+}
+
+var
+    a , b, c : integer;
+begin
+    read(a, b);
+    c := a - b;
+    writeln(c);  
+end.
---
+++ firstProgram.pas
@@ -0,0 +1,6 @@
+{
+    第一个程序
+}
+begin
+    writeln('I love programming!');
+end.
